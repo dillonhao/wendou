4b5a6d9100d9dd87ae0d18a205ab647757e8f9b3
dillonhao <wanghao62@163.com>
2018-01-23 07:15:45 +0800

    add fundamental information

--- Macroeconomics.py
+++ Macroeconomics.py
@@ -11,3 +11,9 @@ class MacroEconomics(object):
         self.__starttime = (datetime.datetime.now() - datetime.timedelta(days=self.__timewindow)).strftime('%Y-%m-%d')
         # __starttime is max time of entry point
         self.__endtime = datetime.datetime.now().strftime('%Y-%m-%d')
+        profit = ts.get_profit_data(2014, 3)
+        report = ts.get_report_data(2014, 3)
+        operation = ts.get_operation_data(2014, 3)
+        growth = ts.get_growth_data(2014, 3)
+        debtpay = ts.get_debtpaying_data(2014, 3)
+        cashflow = ts.get_cashflow_data(2014, 3)
