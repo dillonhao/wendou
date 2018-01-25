14026163571a08b1c6b266540d87ba63562ed597
dillonhao <wanghao62@163.com>
2018-01-25 18:56:25 +0800

    All problem solved only the multi target to be fixed

--- DataAssemble.py
+++ DataAssemble.py
@@ -1,16 +1,27 @@
 from TaMainFrame import TaMainFrame
 from IndexMainFrame import IndexMainFrame
 from TickMainFrame import TickMainFrame
+import pandas as pd
 
+#IndexFrame = IndexMainFrame('000001', 730, 30, 90, 0.1)
+#D2 = IndexFrame.data_assemble(alist)
 
 alist = [5, 10, 30, 60, 90]
-TaFrame = TaMainFrame('600519', 730, 30, 90, 0.1)
-D1 = TaFrame.Pattern_Recognition_Functions()
+ticklist = ['600000', '600008', '600009', '600010', '600011', '600015', '600016', '600018', '600019', '600021', '600023', '600028', '600029', '600030', '600031', '600036', '600038', '600048', '600050', '600061', '600066', '600068', '600074', '600085', '600089', '600100', '600104', '600109', '600111', '600115', '002558', '002572', '002594', '002601', '002602', '002608', '002624', '002673', '002714', '002736', '002739', '002797', '002831', '002839', '002841', '300003', '300015', '300017', '300024', '300027', '300033', '300059', '300070', '300072', '300122', '300124', '300136', '300144', '300251', '300315']
+#ticklist = ['600000', '600008']
+i = int(0)
+for value in ticklist:
+    TaFrame = TaMainFrame(value, 730, 30, 90, 0.2)
+    TickFrame = TickMainFrame(value, 730, 30, 90, 0.2)
+    D1 = TaFrame.Pattern_Recognition_Functions()
+    D3 = TickFrame.data_assemble(alist)
+    if i==0:
+        print('im zero')
+        df = pd.concat([D1,D3],axis=1)
+        out = df
+    else:
+        df = pd.concat([D1, D3], axis=1)
+        out = pd.concat([out,df])
+    i = i+1
 
-IndexFrame = IndexMainFrame('000001', 730, 30, 90, 0.1)
-D2 = IndexFrame.data_assemble(alist)
-#
-TickFrame = TickMainFrame('600519', 730, 30, 90, 0.1)
-D3 = TickFrame.data_assemble(alist)
-
-d4 = pd.concat([D1,D2,D3],axis=1)
+out.to_csv('out.csv')
