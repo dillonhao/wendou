9beae7933a96ae0975431859f349492f38351a75
dillonhao <wanghao62@163.com>
2018-02-05 17:30:04 +0800

    Function is good

--- WLX_Adaptive_Function.py
+++ WLX_Adaptive_Function.py
@@ -1,4 +1,43 @@
 import pandas as pd
 import numpy as np
+from math import log
 
-def memb(x, a, b, c):
+
+def AvgReturn(tick, n, m):
+    # defind the x = ln(x1/x2) which is the return of price. return the series data.
+    # n is small than m, if not ,convert
+    if n > m:
+        tmp = n
+        n = m
+        m = tmp
+    x = pd.Series(index=tick.index)
+    size = tick.size
+    for i, value in enumerate(tick):
+        x.iloc[size - i - 1] = log(tick[size - i - n:size - i].mean() / tick[size - i - m:size - i].mean())
+    return x
+
+
+def meb(x, a, b, c):
+    # return the membership of PL,PM,PS,AZ,NS,NM,NL
+    if x <= a:
+        return 0
+    if (x > a) and (x <= b):
+        return (x-a)/(b-a)
+    if (x > b) and (x <= c):
+        return (c-x)/(c-b)
+    if x > c:
+        return 0
+    if a == b:
+        if x <= b:
+            return 1
+        if (x > b) and (x <= c):
+            return(c-x)/(c-b)
+        if x > c:
+            return 0
+    if b == c:
+        if x <= a:
+            return 0
+        if (x > a) and (x <= b):
+            return (x-a)/(b-a);
+        if x > b:
+            return 1;
--- WLX_Following.py
+++ WLX_Following.py
@@ -1,2 +1,7 @@
 import pandas as pd
 import numpy as np
+
+n = 1
+m = 5
+C = 0.01
+
